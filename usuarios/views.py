d76bca8f9bb8d8231a2abac6957576c120d45a75
xJohn <jhfs632@gmail.com>
2023-10-07 03:43:54 -0300

    a

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:    
+            messages.add_message(request, constants.ERROR, 'Confirmar senha corretamente!')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha precisa ter mais que 6 caracteres!')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            # Username deve ser único!
+            user = User.objects.create_user(
+                first_name=primeiro_nome,
+                last_name=ultimo_nome,
+                username=username,
+                email=email,
+                password=senha,
+            )
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado corretamente!')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro do Sistema! Solicitar suporte!')
+            return redirect('/usuarios/cadastro')
+
+
+        return redirect('/usuarios/cadastro')
+
+def logar(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login(request, user)
+						# Acontecerá um erro ao redirecionar por enquanto, resolveremos nos próximos passos
+            return redirect('/')
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuario ou senha inválidos')
+            return redirect('/usuarios/login')
--- vitalab/settings.py
+++ vitalab/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'exames',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,15 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# mensagens Django
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
