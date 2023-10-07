c79d886ce1c1399723db9f8a7c78dc9cc580cd18
xJohn <jhfs632@gmail.com>
2023-10-07 15:37:30 -0300

    a

--- exames/models.py
+++ exames/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+from secrets import token_urlsafe
+from django.utils import timezone
+from datetime import timedelta
+
 
 class TiposExames(models.Model):
     tipo_choices = (
-        ('I', 'Exame de imagem'),
-        ('S', 'Exame de Sangue')
+        ('I', 'Exame de Imagem'),
+        ('S', 'Exame de Sangue'),
     )
     nome = models.CharField(max_length=50)
     tipo = models.CharField(max_length=2, choices=tipo_choices)
@@ -26,13 +28,23 @@ class SolicitacaoExame(models.Model):
     )
     usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     exame = models.ForeignKey(TiposExames, on_delete=models.DO_NOTHING)
-    status = models.CharField(max_length=2, choices=choice_status)
+    status = models.CharField(max_length=1, choices=choice_status)
     resultado = models.FileField(upload_to="resultados", null=True, blank=True)
     requer_senha = models.BooleanField(default=False)
     senha = models.CharField(max_length=6, null=True, blank=True)
 
     def __str__(self):
         return f'{self.usuario} | {self.exame.nome}'
+
+    def badge_template(self):
+        if self.status == 'E':
+            classes_css = 'bg-warning text-dark'
+            texto = "Em análise..."
+        elif self.status == 'F':
+            classes_css = 'bg-success'
+            texto = "Finalizado"
+        
+        return mark_safe(f"<span class='badge bg-primary {classes_css}'>{texto}</span>")
 
 class PedidosExames(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
@@ -42,3 +54,32 @@ class PedidosExames(models.Model):
 
     def __str__(self):
         return f'{self.usuario} | {self.data}'
+    
+
+class AcessoMedico(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    identificacao = models.CharField(max_length=50)
+    tempo_de_acesso = models.IntegerField() # Em horas
+    criado_em = models.DateTimeField()
+    data_exames_iniciais = models.DateField()
+    data_exames_finais = models.DateField()
+    token = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.token
+    
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = token_urlsafe(6)
+
+        super(AcessoMedico, self).save(*args, **kwargs)    
+
+    @property
+    def status(self):
+        return 'Expirado' if timezone.now() > (self.criado_em + timedelta(hours=self.tempo_de_acesso)) else 'Ativo'
+  
+    @property
+    def url(self):
+        #TODO: reverse
+        return f"http://127.0.0.1:8000/exames/acesso_medico/{self.token}"
+    
