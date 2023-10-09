38679415b4b1d3297bee71e3339d429a977ad31a
xJohn <jhfs632@gmail.com>
2023-10-08 23:24:44 -0300

    a

--- empresarial/views.py
+++ empresarial/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.messages import constants
 from django.contrib.auth.models import User
+from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import Value
 from django.db.models.functions import Concat
-from django.contrib.admin.views.decorators import staff_member_required
 from exames.models import SolicitacaoExame
 from django.http import FileResponse
+from .utils import gerar_pdf_exames, gerar_senha_aleatoria
 
 @staff_member_required 
 def gerenciar_clientes(request):
@@ -37,6 +40,40 @@ def exame_cliente(request, exame_id):
 @staff_member_required 
 def proxy_pdf(request, exame_id):
     exame = SolicitacaoExame.objects.get(id=exame_id)
-
     response = exame.resultado.open()
     return FileResponse(response)
+
+@staff_member_required
+def gerar_senha(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+
+    if exame.senha:
+        # Baixar o documento da senha já existente
+        return FileResponse(gerar_pdf_exames(exame.exame.nome, exame.usuario, exame.senha), filename="token.pdf")
+    
+    senha = gerar_senha_aleatoria(9)
+    exame.senha = senha
+    exame.save()
+    return FileResponse(gerar_pdf_exames(exame.exame.nome, exame.usuario, exame.senha), filename="token.pdf")
+
+@staff_member_required 
+def alterar_dados_exame(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+
+    pdf = request.FILES.get('resultado')
+    status = request.POST.get('status')
+    requer_senha = request.POST.get('requer_senha')
+    
+    if requer_senha and (not exame.senha):
+        messages.add_message(request, constants.ERROR, 'PDF sem Senha criada! Não pode exigir senha!')
+        return redirect(f'/empresarial/exame_cliente/{exame_id}')
+    
+    exame.requer_senha = True if requer_senha else False
+
+    if pdf:
+        exame.resultado = pdf
+        
+    exame.status = status
+    exame.save()
+    messages.add_message(request, constants.SUCCESS, 'Dados do exame alterados!')
+    return redirect(f'/empresarial/exame_cliente/{exame_id}')
